0f00ba6e4e91358059017861d453cfb02b5193fb
Alessandra bergamasque de Lima <ale_bergamasque@hotmail.com>
2023-05-11 11:53:54 -0300

    raiz do projeto

---
+++ servico-mensageria/src/main/java/br/com/servicomensageria/ServicoMensageriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.servicomensageria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoMensageriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoMensageriaApplication.class, args);
+	}
+
+}
---
+++ servico-mensageria/src/test/java/br/com/servicomensageria/ServicoMensageriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.servicomensageria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoMensageriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
